982f21f3692852aa3e4b065cb0005750ba3a24aa
fajartc02 <fajartc02@gmail.com>
2018-05-09 23:26:18 +0700

    submit ubahHuruf.js

--- ubahHuruf.js
+++ ubahHuruf.js
@@ -1,6 +1,5 @@
 function ubahHuruf(kata) {
     // // you can only write your code here!
-    // // var aZ = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     var aZ = 'abcdefghijklmnopqrstuvwxyza'
     var kataBaru = ''
     
@@ -14,8 +13,9 @@ function ubahHuruf(kata) {
   }
   
   // TEST CASES
-  console.log(ubahHuruf('wow')); // xpx
-  console.log(ubahHuruf('developer')); // efwfmpqfs
-  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
-  console.log(ubahHuruf('keren')); // lfsfo
-  console.log(ubahHuruf('semangat')); // tfnbohbu
+  // console.log(ubahHuruf('wow')); // xpx
+  // console.log(ubahHuruf('developer')); // efwfmpqfs
+  // console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+  // console.log(ubahHuruf('keren')); // lfsfo
+  // console.log(ubahHuruf('semangat')); // tfnbohbu
+  // console.log(ubahHuruf('zaza'))
