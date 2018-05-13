d1c6d7ab7f9453c65311aa0158715dad089a27a3
fajartc02 <fajartc02@gmail.com>
2018-05-13 16:41:04 +0700

    submit mostFrequentLargest.js

--- mostFrequentLargest.js
+++ mostFrequentLargest.js
@@ -12,23 +12,25 @@ function sorting(arrNumber) {
     }
     // console.log(arrNumber)
     return arrNumber
-    return arrNumber.length
   }
-  
   function getTotal(arrNumber) {
     // code di sini
-    var leng = arrNumber.length
     // console.log(arrNumber)//out: [ 8, 8, 8, 6, 5, 4, 4, 2 ]
-    var counter = 0
-    if (arrNumber.length == 0) {//arrNumber.length = 8
-        return 0
-    } else if (arrNumber.length > 0) {
-        if (arrNumber[0] == arrNumber[0]) {
-            counter++
-            console.log(counter)
-            return getTotal(arrNumber - 1)
+    var counter = []
+    var conIndex = []
+    for (var i = 0; i < arrNumber.length; i++) {
+        var index = counter.indexOf(arrNumber[i])
+        if(index === -1) {
+            counter.push(arrNumber[i])
+            conIndex.push(1)
+        } else if (index !== -1) {
+            conIndex[index] = conIndex[index] + 1
         }
     }
+    if (arrNumber == 0) {
+        return []
+    }
+    return 'angka paling besar adalah ' + arrNumber[0] + ' dan jumlah kemunculannya sebanyak ' + conIndex[0] + ' kali'
   }
   
   function mostFrequentLargestNumbers(arrNumber) {
@@ -40,11 +42,11 @@ function sorting(arrNumber) {
   console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
   //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
   
-//   console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+  console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
 //   //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
   
-//   console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+  console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
 //   //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
   
-//   console.log(mostFrequentLargestNumbers([]));
+  console.log(mostFrequentLargestNumbers([]));
   //''
