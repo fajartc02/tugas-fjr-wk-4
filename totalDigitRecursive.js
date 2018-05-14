7e335ca40037bc83c436ee19762c7d1409e7e6ad
fajartc02 <fajartc02@gmail.com>
2018-05-14 20:06:17 +0700

    submit totalDigitRecursive.js

--- totalDigitRecursive.js
+++ totalDigitRecursive.js
@@ -3,7 +3,7 @@ function totalDigitRekursif(angka) {
   if (angka === 0) {
     return 0
   } else {   
-    return (angka % 10) + totalDigitRekursif(Math.trunc(angka/10)) // out : 2 + 1 + 5 = 8
+    return (angka % 10) + totalDigitRekursif(Math.floor/*untuk membulatkan angka kebawah*/(angka/10)) // out : 2 + 1 + 5 = 8
     // 2 + totalDigitRecursive(512/10 = 51.2) 51<== Math.trunc
     // 2 + (51 % 10) = 1 + totalDigitRecursive(51 / 10 = 5.1) 5 <== Math.trunc
     // 2 + 1 + 5 + totalDigitRecursive(5 / 10 = 0.5) 0<== Math.trunc
